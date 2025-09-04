bdfa0e550e705874ceb47caaa6c4e364de4cb0bd
retin <voicemin78@gmail.com>
2025-09-04 18:13:01 +0900

    branch naming 1-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>NEXT JS</h1>
-        <h2>branch: test1</h2>
-        <h3>before branch: test1-1</h3>
+        <h2>branch: test1-1</h2>
+        <h3>before branch: test1</h3>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         by. retin
